df109c5f55f6792dfd79055524728206f3a05247
Michael Cook <michael@mrcook.uk>
2016-11-16 21:16:25 +0100

    Replaces file header license with a more concise version

--- src/create.c
+++ src/create.c
@@ -3,20 +3,9 @@
 // Copyright (C) 1989-2008 James E. Wilson, Robert A. Koeneke,
 //                         David J. Grabiner
 //
-// This file is part of Umoria.
-//
-// Umoria is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Umoria is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Umoria.  If not, see <http://www.gnu.org/licenses/>.
+// Umoria is free software released under a GPL v2 license and comes with
+// ABSOLUTELY NO WARRANTY. See https://www.gnu.org/licenses/gpl-2.0.html
+// for further details.
 
 #include "headers.h"
 
