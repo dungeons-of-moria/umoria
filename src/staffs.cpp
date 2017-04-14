94ba12449d041f6dcb7dcdf42cf0e4e6ed239b2f
Michael Cook <michael@mrcook.uk>
2017-04-14 11:43:15 +0200

    Refactor staffs.cpp
    Perform Extract Method, rename variables, etc.

--- src/staffs.cpp
+++ src/staffs.cpp
@@ -9,159 +9,154 @@
 #include "headers.h"
 #include "externs.h"
 
-// Use a staff. -RAK-
-void use() {
-    free_turn_flag = true;
 
+static bool isCarryingStaff(int *j, int *k) {
     if (inven_ctr == 0) {
         msg_print("But you are not carrying anything.");
-        return;
+        return false;
     }
 
-    int j, k;
-    if (!find_range(TV_STAFF, TV_NEVER, &j, &k)) {
+    if (!find_range(TV_STAFF, TV_NEVER, j, k)) {
         msg_print("You are not carrying any staffs.");
-        return;
+        return false;
     }
 
-    int item_val;
-    if (!get_item(&item_val, "Use which staff?", j, k, CNIL, CNIL)) {
-        return;
-    }
+    return true;
+}
 
-    free_turn_flag = false;
-
-    inven_type *i_ptr = &inventory[item_val];
-    struct player_type::misc *m_ptr = &py.misc;
-
-    int chance = m_ptr->save + stat_adj(A_INT) - (int) i_ptr->level - 5 + (class_level_adj[m_ptr->pclass][CLA_DEVICE] * m_ptr->lev / 3);
+static bool canUseStaff(inven_type *staff_ptr) {
+    int chance = py.misc.save + stat_adj(A_INT) - (int) staff_ptr->level - 5 + (class_level_adj[py.misc.pclass][CLA_DEVICE] * py.misc.lev / 3);
 
     if (py.flags.confused > 0) {
         chance = chance / 2;
     }
 
+    // Give everyone a slight chance
     if (chance < USE_DEVICE && randint(USE_DEVICE - chance + 1) == 1) {
-        chance = USE_DEVICE; // Give everyone a slight chance
+        chance = USE_DEVICE;
     }
 
-    if (chance <= 0) {
+    if (chance < 1) {
         chance = 1;
     }
 
     if (randint(chance) < USE_DEVICE) {
         msg_print("You failed to use the staff properly.");
-        return;
+        return false;
     }
 
-    if (i_ptr->p1 < 1) {
+    if (staff_ptr->p1 < 1) {
         msg_print("The staff has no charges left.");
-        if (!known2_p(i_ptr)) {
-            add_inscribe(i_ptr, ID_EMPTY);
+        if (!known2_p(staff_ptr)) {
+            add_inscribe(staff_ptr, ID_EMPTY);
         }
-        return;
+        return false;
     }
 
-    uint32_t i = i_ptr->flags;
-    (i_ptr->p1)--;
+    return true;
+}
 
-    int y, x;
-    bool ident = false;
+static bool dischargeStaff(inven_type *staff_ptr) {
+    bool identified = false;
 
-    while (i != 0) {
-        int pos = bit_pos(&i) + 1;
+    staff_ptr->p1--;
 
-        // Staffs.
-        switch (pos) {
+    uint32_t flags = staff_ptr->flags;
+    while (flags != 0) {
+        int staff_type = bit_pos(&flags) + 1;
+
+        switch (staff_type) {
             case 1:
-                ident = light_area(char_row, char_col);
+                identified = light_area(char_row, char_col);
                 break;
             case 2:
-                ident = detect_sdoor();
+                identified = detect_sdoor();
                 break;
             case 3:
-                ident = detect_trap();
+                identified = detect_trap();
                 break;
             case 4:
-                ident = detect_treasure();
+                identified = detect_treasure();
                 break;
             case 5:
-                ident = detect_object();
+                identified = detect_object();
                 break;
             case 6:
                 teleport(100);
-                ident = true;
+                identified = true;
                 break;
             case 7:
-                ident = true;
+                identified = true;
                 earthquake();
                 break;
             case 8:
-                ident = false;
-                for (k = 0; k < randint(4); k++) {
-                    y = char_row;
-                    x = char_col;
-                    ident |= summon_monster(&y, &x, false);
+                identified = false;
+
+                for (int k = 0; k < randint(4); k++) {
+                    int y = char_row;
+                    int x = char_col;
+                    identified |= summon_monster(&y, &x, false);
                 }
                 break;
             case 10:
-                ident = true;
+                identified = true;
                 destroy_area(char_row, char_col);
                 break;
             case 11:
-                ident = true;
+                identified = true;
                 starlite(char_row, char_col);
                 break;
             case 12:
-                ident = speed_monsters(1);
+                identified = speed_monsters(1);
                 break;
             case 13:
-                ident = speed_monsters(-1);
+                identified = speed_monsters(-1);
                 break;
             case 14:
-                ident = sleep_monsters2();
+                identified = sleep_monsters2();
                 break;
             case 15:
-                ident = hp_player(randint(8));
+                identified = hp_player(randint(8));
                 break;
             case 16:
-                ident = detect_invisible();
+                identified = detect_invisible();
                 break;
             case 17:
                 if (py.flags.fast == 0) {
-                    ident = true;
+                    identified = true;
                 }
                 py.flags.fast += randint(30) + 15;
                 break;
             case 18:
                 if (py.flags.slow == 0) {
-                    ident = true;
+                    identified = true;
                 }
                 py.flags.slow += randint(30) + 15;
                 break;
             case 19:
-                ident = mass_poly();
+                identified = mass_poly();
                 break;
             case 20:
                 if (remove_curse()) {
                     if (py.flags.blind < 1) {
                         msg_print("The staff glows blue for a moment..");
                     }
-                    ident = true;
+                    identified = true;
                 }
                 break;
             case 21:
-                ident = detect_evil();
+                identified = detect_evil();
                 break;
             case 22:
                 if (cure_blindness() || cure_poison() || cure_confusion()) {
-                    ident = true;
+                    identified = true;
                 }
                 break;
             case 23:
-                ident = dispel_creature(CD_EVIL, 60);
+                identified = dispel_creature(CD_EVIL, 60);
                 break;
             case 25:
-                ident = unlight_area(char_row, char_col);
+                identified = unlight_area(char_row, char_col);
                 break;
             case 32:
                 // store bought flag
@@ -170,26 +165,49 @@ void use() {
                 msg_print("Internal error in staffs()");
                 break;
         }
-        // End of staff actions.
     }
 
-    if (ident) {
-        if (!known1_p(i_ptr)) {
-            m_ptr = &py.misc;
+    return identified;
+}
 
+// Use a staff. -RAK-
+void use() {
+    int j, k;
+
+    free_turn_flag = true;
+
+    if (!isCarryingStaff(&j, &k)) {
+        return;
+    }
+
+    int staff_id;
+    if (!get_item(&staff_id, "Use which staff?", j, k, CNIL, CNIL)) {
+        return;
+    }
+
+    // From here on player uses up a turn
+    free_turn_flag = false;
+
+    inven_type *staff_ptr = &inventory[staff_id];
+
+    if (!canUseStaff(staff_ptr)) {
+        return;
+    }
+
+    bool identified = dischargeStaff(staff_ptr);
+
+    if (identified) {
+        if (!known1_p(staff_ptr)) {
             // round half-way case up
-            m_ptr->exp += (i_ptr->level + (m_ptr->lev >> 1)) / m_ptr->lev;
+            py.misc.exp += (staff_ptr->level + (py.misc.lev >> 1)) / py.misc.lev;
 
             prt_experience();
 
-            identify(&item_val);
-
-            // NOTE: this is never read after this, so commenting out. -MRC-
-            // i_ptr = &inventory[item_val];
+            identify(&staff_id);
         }
-    } else if (!known1_p(i_ptr)) {
-        sample(i_ptr);
+    } else if (!known1_p(staff_ptr)) {
+        sample(staff_ptr);
     }
 
-    desc_charges(item_val);
+    desc_charges(staff_id);
 }
