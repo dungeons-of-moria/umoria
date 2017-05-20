4101acd3f1c0d59bc2f14f78716ec610bba24cbc
Michael Cook <michael@mrcook.uk>
2017-05-20 09:46:28 +0200

    Small refactor of misc4.cpp

--- src/misc4.cpp
+++ src/misc4.cpp
@@ -17,23 +17,26 @@ void scribe_object() {
     }
 
     int item_val;
-
     if (get_item(&item_val, "Which one? ", 0, INVEN_ARRAY_SIZE, CNIL, CNIL)) {
         bigvtype out_val, tmp_str;
 
         objdes(tmp_str, &inventory[item_val], true);
         (void) sprintf(out_val, "Inscribing %s", tmp_str);
         msg_print(out_val);
+
         if (inventory[item_val].inscrip[0] != '\0') {
             (void) sprintf(out_val, "Replace %s New inscription:", inventory[item_val].inscrip);
         } else {
             (void) strcpy(out_val, "Inscription: ");
         }
+
         int j = 78 - (int) strlen(tmp_str);
         if (j > 12) {
             j = 12;
         }
+
         prt(out_val, 0, 0);
+
         if (get_string(out_val, 0, (int) strlen(out_val), j)) {
             inscribe(&inventory[item_val], out_val);
         }
@@ -62,19 +65,19 @@ void check_view() {
     // Move the light source
     move_light(char_row, char_col, char_row, char_col);
 
+    // A room of light should be lit.
     if (c_ptr->fval == LIGHT_FLOOR) {
-        // A room of light should be lit.
-
         if (py.flags.blind < 1 && !c_ptr->pl) {
             light_room(char_row, char_col);
         }
-    } else if (c_ptr->lr && py.flags.blind < 1) {
-        // In doorway of light-room?
+        return;
+    }
 
-        for (int i = (char_row - 1); i <= (char_row + 1); i++) {
-            for (int j = (char_col - 1); j <= (char_col + 1); j++) {
-                cave_type *d_ptr = &cave[i][j];
-                if (d_ptr->fval == LIGHT_FLOOR && !d_ptr->pl) {
+    // In doorway of light-room?
+    if (c_ptr->lr && py.flags.blind < 1) {
+        for (int i = char_row - 1; i <= char_row + 1; i++) {
+            for (int j = char_col - 1; j <= char_col + 1; j++) {
+                if (cave[i][j].fval == LIGHT_FLOOR && !cave[i][j].pl) {
                     light_room(i, j);
                 }
             }
