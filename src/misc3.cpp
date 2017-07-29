c9274cca600c975f46b624e9580b0cd796d9af23
Sebastian Martin Dicke <Sebastianmartindicke [@] gmx [.] de>
2017-07-29 14:39:30 +0200

    minor reverse

--- src/misc3.cpp
+++ src/misc3.cpp
@@ -10,7 +10,7 @@
 #include "externs.h"
 
 static const char *stat_names[] = {"STR : ", "INT : ", "WIS : ", "DEX : ", "CON : ", "CHR : ",};
-static constexpr int32_t BLANK_LENGTH = 24;
+#define BLANK_LENGTH 24
 static char blank_string[] = "                        ";
 
 static int spellChanceOfSuccess(int spell);
